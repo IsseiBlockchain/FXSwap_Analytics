7210f64b3132ff3d02fbe7821ec5a63ebde3a7b2
Issei Kumagai <Kumagaiissei@hotmail.com>
2022-07-15 16:26:57 +0800

    update

---
+++ build/precache-manifest.2befef22d5d2ba4be3737738e5b531f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2511dfb1fa329d3dea767ede957fa10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c5e60558bec4f4a92f",
+    "url": "/static/js/2.1a0f4f55.chunk.js"
+  },
+  {
+    "revision": "d255601c39ba9ea8be3f576f2fb7e1d5",
+    "url": "/static/js/2.1a0f4f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a327b68ffa09bf683ce",
+    "url": "/static/js/main.344da104.chunk.js"
+  },
+  {
+    "revision": "c7805d0a662790c235f8",
+    "url": "/static/js/runtime-main.c0105f97.js"
+  },
+  {
+    "revision": "5fc0c9bdb40040928ed73dbbcecd62a3",
+    "url": "/static/media/eth.5fc0c9bd.png"
+  },
+  {
+    "revision": "27a64f2ce23201015201449eeed6449f",
+    "url": "/static/media/logo.27a64f2c.svg"
+  },
+  {
+    "revision": "325adc6a67398a44d8fa97a7589c1dad",
+    "url": "/static/media/logo_white.325adc6a.svg"
+  },
+  {
+    "revision": "9914390fc6b153e39de552594e0b1d3a",
+    "url": "/static/media/wordmark_white.9914390f.svg"
+  }
+]);
